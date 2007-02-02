562bdd7433df2673928e6b8ce0fb5472ce83ded4
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2007-02-02 11:02:33 +0000

    added sub-directory capability

--- test/TestTFileServiceAnalyzer.cc
+++ test/TestTFileServiceAnalyzer.cc
@@ -24,12 +24,18 @@ TestTFileServiceAnalyzer::TestTFileServiceAnalyzer( const ParameterSet & cfg ) :
   dir1_( cfg.getParameter<string>( "dir1" ) ),
   dir2_( cfg.getParameter<string>( "dir2" ) ) {
   Service<TFileService> fs;
-  if ( ! dir1_.empty() )
-    fs->cd( dir1_ );
-  h_test1 = fs->make<TH1F>( "test1"  , "test histogram #1", 100,  0., 100. );
-  if ( ! dir2_.empty() )
-    fs->cd( dir2_ );
-  h_test2 = fs->make<TH1F>( "test2"  , "test histogram #2", 100,  0., 100. );
+  if ( dir1_.empty() ) {
+    h_test1 = fs->make<TH1F>( "test1"  , "test histogram #1", 100,  0., 100. );
+  } else {
+    TFileDirectory dir1 = fs->mkdir( dir1_ );
+    h_test1 = dir1.make<TH1F>( "test1"  , "test histogram #1", 100,  0., 100. );
+  }
+  if ( dir2_.empty() ) {
+    h_test2 = fs->make<TH1F>( "test2"  , "test histogram #2", 100,  0., 100. );
+  } else {
+    TFileDirectory dir2 = fs->mkdir( dir2_ );
+    h_test2 = dir2.make<TH1F>( "test2"  , "test histogram #2", 100,  0., 100. );
+  }
 }
 
 void TestTFileServiceAnalyzer::analyze( const Event& evt, const EventSetup& ) {
