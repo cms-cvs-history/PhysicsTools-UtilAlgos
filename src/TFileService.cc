cc4fba3620538efa8e45157e47db78e8e1eb376e
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2007-05-11 11:11:34 +0000

    patch for 1_3_X

--- src/TFileService.cc
+++ src/TFileService.cc
@@ -1,5 +1,5 @@
 #include "PhysicsTools/UtilAlgos/interface/TFileService.h"
-#include "DataFormats/Provenance/interface/ModuleDescription.h"
+#include "DataFormats/Common/interface/ModuleDescription.h"
 #include "FWCore/ServiceRegistry/interface/ActivityRegistry.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "TFile.h"
@@ -11,7 +11,7 @@ TFileService::TFileService( const ParameterSet & cfg, ActivityRegistry & r ) :
   file_( TFileDirectory::file_ ) {
   r.watchPreModuleConstruction( this, & TFileService::setDirectoryName ); 
   r.watchPreModule( this, & TFileService::setDirectoryName ); 
-  r.watchPreModuleBeginJob( this, & TFileService::setDirectoryName ); 
+  //  r.watchPreModuleBeginJob( this, & TFileService::setDirectoryName ); 
 }
 
 TFileService::~TFileService() {
