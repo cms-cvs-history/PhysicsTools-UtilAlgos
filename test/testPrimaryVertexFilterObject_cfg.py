0aad14cc5fb51960195f64331de32e85a5100c3f
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2011-04-13 17:07:44 +0000

    added to unit tests

---
+++ test/testPrimaryVertexFilterObject_cfg.py
@@ -0,0 +1,25 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Test")
+
+process.source = cms.Source("PoolSource",
+  fileNames = cms.untracked.vstring(
+    "/store/relval/CMSSW_4_2_0/RelValZTT/GEN-SIM-RECO/START42_V9-v1/0054/107DB9B4-7D5E-E011-91E9-001A92810AEA.root"
+  )
+)
+## Maximal Number of Events
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+
+process.MessageLogger = cms.Service("MessageLogger")
+
+process.primaryVertexObjectFilter = cms.EDFilter("PrimaryVertexObjectFilter",
+  src   = cms.InputTag("offlinePrimaryVertices"),
+  filterParams = cms.PSet(
+    minNdof = cms.double( 4 ),
+    maxZ    = cms.double( 2 ),
+    maxRho  = cms.double(0.2)
+  )
+)
+
+process.p = cms.Path(process.primaryVertexObjectFilter)
+
