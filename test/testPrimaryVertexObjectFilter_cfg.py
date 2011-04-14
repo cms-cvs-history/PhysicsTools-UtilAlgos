a356e2fc3237dfc771905367022493d9757a1ede
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2011-04-14 10:52:20 +0000

    cleanup of documentation and module structures

--- test/testPrimaryVertexObjectFilter_cfg.py
+++ test/testPrimaryVertexObjectFilter_cfg.py
@@ -7,11 +7,18 @@ process.source = cms.Source("PoolSource",
     "/store/relval/CMSSW_4_2_0/RelValZTT/GEN-SIM-RECO/START42_V9-v1/0054/107DB9B4-7D5E-E011-91E9-001A92810AEA.root"
   )
 )
-## Maximal Number of Events
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 process.MessageLogger = cms.Service("MessageLogger")
 
+## ---
+## This is an example of the use of the EDFilterObjectWrapper to exploit C++ selector classes as defined
+## in the PhysicsTools/SelectorUtils package wrapping them into an EDFilter that in addition creates a
+## collection of objects passing the selection criteria. You can find the implementation of the EDProducer
+## in PhysicsTools/UtilAlgos/plugins/PrimaryVertexFilter.cc. You can find the EDfilterWrapper class in
+## PhysicsTools/UtilAlgos/interface/EDFilterObjectWrapper.h. The class that has been exploited here is
+## the PVObjectSelector class of the PhysicsTools/SelectorUtils package.You cna find the input parameters
+## there. 
 process.primaryVertexObjectFilter = cms.EDFilter("PrimaryVertexObjectFilter",
   src   = cms.InputTag("offlinePrimaryVertices"),
   filterParams = cms.PSet(
