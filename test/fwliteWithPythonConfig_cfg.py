115df9543be3157bc8612033252906f1516a4106
cmsbuild <>
2013-04-11 16:44:23 +0000

    This commit was manufactured by cvs2git to create tag 'PhysicsTools- UtilAlgos-CMSSW_5_3_10'.
    Sprout from master 2011-04-14 10:52:20 UTC Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch> 'cleanup of documentation and module structures'
    Cherrypick from PhysicsTools-UtilAlgos-B5_3_X 2013-04-11 16:44:22 UTC Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch> 'Update RelVal input.':
        test/cmsswWithPythonConfig_cfg.py
        test/fwliteWithPythonConfig_cfg.py
        test/select_error_events_cfg.py
        test/testPrimaryVertexFilter_cfg.py
        test/testPrimaryVertexObjectFilter_cfg.py

--- test/fwliteWithPythonConfig_cfg.py
+++ test/fwliteWithPythonConfig_cfg.py
@@ -3,11 +3,11 @@ import FWCore.ParameterSet.Config as cms
 process = cms.PSet()
 
 process.fwliteInput = cms.PSet(
-    fileNames   = cms.vstring('rfio:///castor/cern.ch/cms/store/relval/CMSSW_4_1_3/RelValTTbar/GEN-SIM-RECO/START311_V2-v1/0037/648B6AA5-C751-E011-8208-001A928116C6.root'), ## mandatory
+    fileNames   = cms.vstring('root://eoscms//eos/cms/store/relval/CMSSW_5_3_6-START53_V14/RelValZTT/GEN-SIM-RECO/v2/00000/4E4AD1B8-FC29-E211-B998-001A928116B4.root'), ## mandatory
     maxEvents   = cms.int32(100),                            ## optional
     outputEvery = cms.uint32(10),                            ## optional
 )
-    
+
 process.fwliteOutput = cms.PSet(
     fileName  = cms.string('analyzeFWLiteHistograms.root'),  ## mandatory
 )
